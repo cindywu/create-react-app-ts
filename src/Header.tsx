763f56f2269143e4e623333828a6a212328a7644
cindy <ciindy.wu@gmail.com>
2021-01-25 13:00:48 -1000

    Adds a counter

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 interface HeaderProps {
-  buttonText: string;
-  
+  buttonText: string
 }
 
 export default function Header({ buttonText }: HeaderProps) {
+  const [count, setCount] = useState(0)
+  
+  const increment = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div>
       <h1>Header</h1>
-      <button>{buttonText}</button>
+      <button onClick={increment}>{buttonText}</button>
+      <p>{count}</p>
     </div>
   )
 }
